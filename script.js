9714fdab75195ac79c3cfc60e0e4640b78893a97
Thiago Santos <thiagosantos77@hotmail.com>
2023-12-14 01:55:11 -0300

    Agumas atualizaçôes importantes

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ function mostrarDetalhes(produto) {
 
     switch (produto) {
         case "produto1":
-            modalTitulo.innerText = "Tênis Nike just do it. R$99,90 QUALQUER MODELO";
-            modalDescricao.innerHTML = "Tênis Disponivel para entrega, e diversos outros modelos, para celebrar o fim de ano. Disponivel a pronta entrega no nosso Whatsaapp , CLICK NO BOTÃO COMPRAR AGORA";
-            modalBotoes.innerHTML = "<button onclick='saibaMais(\"#\")'>Link Shopee</button> <button onclick='compreAgora(\"https://wa.me/message/FFU5OZUU7HDRL1\")'>Compre Agora</button>";
+            modalTitulo.innerText = "PROMOÇÃO DE TÊNIS. R$99,90 QUALQUER MODELO";
+            modalDescricao.innerHTML = "Tênis Disponivel a PRONTA ENTREGA, e diversos outros modelos, para celebrar o fim de ano. Disponivel a pronta entrega no nosso Whatsaapp , CLICK NO BOTÃO COMPRAR AGORA";
+            modalBotoes.innerHTML = "<button onclick='saibaMais(\"https://wa.me/c/559881286938\")'>CATALOGO</button> <button onclick='compreAgora(\"https://wa.me/message/FFU5OZUU7HDRL1\")'>Compre Agora</button>";
             break;
         case "produto2":
             modalTitulo.innerText = "5 Camisas por Apenas R$100";
             modalDescricao.innerHTML = "5 Camisas por Apenas R$100. POR TEMPO LIMITADO!! È ISSO MESMO ! COMPRE AGORA NA SHOPE OU PEÇA NO WHATSAPP, ENTRAGA GRATIS PARA SÃO LUIS.";
-            modalBotoes.innerHTML = "<button onclick='saibaMais(\https://shp.ee/5e4n9x3\")'>Link Shopee</button> <button onclick='compreAgora(\"https://wa.me/message/FFU5OZUU7HDRL1\")'>Compre Agora</button>";
+            modalBotoes.innerHTML = "<button onclick='saibaMais(\https://shp.ee/5e4n9x3\")'>Link Shopee</button> <button onclick='compreAgora(\"https://wa.me/p/6882087665215068/559881286938\")'>Compre Agora</button>";
             break;
         // Adicione mais casos conforme necessário
     }
